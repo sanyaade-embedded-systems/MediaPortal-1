c088c83980da4dec0a494b59f95cc9e6e23952c2
DJBlu <djblu@djblu.plus.com>
2012-02-07 09:22:27 +0000

    Fixed SQL upgrade file for MSSQL.

--- TvEngine3/TVLibrary/SetupTv/61_upgrade_sqlserver_database.sql
+++ TvEngine3/TVLibrary/SetupTv/61_upgrade_sqlserver_database.sql
@@ -2,15 +2,15 @@ use %TvLibrary%
 GO
  
 ALTER TABLE program
-	ADD seriesId varchar(200)
+  ADD seriesId varchar(200)
 GO
- 
+
 ALTER TABLE program
-	ADD seriesTermination TINYINT
+  ADD seriesTermination TINYINT
 GO
- 
+
 ALTER TABLE schedule
-	ADD seriesId varchar(200)
+  ADD seriesId varchar(200)
 GO
 
 UPDATE Version SET versionNumber=61
